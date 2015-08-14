eb96ada21f58e91f1a3137f1eea4da5a15af8066
wanglijun <wanglj@int-yt.com>
2015-08-14 11:11:03 +0800

    天假录音

--- src/com/ytint/wloaa/activity/AddZhiliangSendActivity.java
+++ src/com/ytint/wloaa/activity/AddZhiliangSendActivity.java
@@ -1,21 +1,39 @@
 package com.ytint.wloaa.activity;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -26,6 +44,7 @@ import com.ab.http.AbStringHttpResponseListener;
 import com.ab.view.ioc.AbIocView;
 import com.ab.view.titlebar.AbTitleBar;
 import com.ytint.wloaa.R;
+import com.ytint.wloaa.activity.ShenpiDetailActivity.MAdapter;
 import com.ytint.wloaa.app.MyApplication;
 import com.ytint.wloaa.app.UIHelper;
 import com.ytint.wloaa.bean.People;
@@ -34,7 +53,7 @@ import com.ytint.wloaa.bean.QunfaInfo;
 import com.ytint.wloaa.bean.URLs;
 
 public class AddZhiliangSendActivity extends AbActivity {
-	String TAG = "AddXiapaiActivity";
+	String TAG = "AddZhiliangSendActivity";
 	private MyApplication application;
 	Context context = null;
 	private String loginKey;
@@ -43,7 +62,7 @@ public class AddZhiliangSendActivity extends AbActivity {
 	private long people = 0;
 	private List<People> peoples;
 	private int from;
-	
+	boolean isLongClick = false;
 	@AbIocView(id = R.id.select_people)
 	Spinner peopleSpinner;
 	@AbIocView(id = R.id.addTask)
@@ -52,49 +71,83 @@ public class AddZhiliangSendActivity extends AbActivity {
 	EditText task_info;
 	@AbIocView(id = R.id.task_tell)
 	EditText task_tell;
-//	@AbIocView(id = R.id.search_close)
-//	TextView search_close;
+	// @AbIocView(id = R.id.search_close)
+	// TextView search_close;
 	@AbIocView(id = R.id.addxiapai_full)
 	LinearLayout addxiapai_full;
+	@AbIocView(id = R.id.addVoice)
+	Button addVoice;
+	@AbIocView(id = R.id.addvoicegridview)
+	GridView addvoicegridview;
+	@AbIocView(id=R.id.horizontalScrollView_addvoice)
+	HorizontalScrollView horizontalScrollView_addvoice;
+	/** 显示语音列表 */
+	private ListView mVoidListView;
+	/** 语音列表适配器 */
+	private MyGridAdapter mAdapter;
+	/** 语音列表 */
+	private List<String> mVoicesList;
+	/** 语音名称列表 */
+	private List<String> mVoicesListname;
+	/** 录音存储路径 */
+	private static final String PATH = "/sdcard/MyVoiceForder/Record/";
+	/** 用于语音播放 */
+	private MediaPlayer mPlayer = null;
+	/** 用于完成录音 */
+	private MediaRecorder mRecorder = null;
+	/** 语音文件保存路径 */
+	private String mFileName = null;
+	/** 语音文件显示名称 */
+	private String mFileNameShow = null;
+	/**
+	 * 列宽
+	 */
+	private int cWidth = 400;
+	/**
+	 * 水平间距
+	 */
+	private int hSpacing = 10;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Intent intent = getIntent();
 		from = Integer.parseInt(intent.getExtras().get("from").toString());
 		AbTitleBar mAbTitleBar = this.getTitleBar();
-		if (from==1) {
+		if (from == 1) {
 			mAbTitleBar.setTitleText("质量检查-下派质检任务");
-		}else if(from==2){
+		} else if (from == 2) {
 			mAbTitleBar.setTitleText("安全检查-下派安检任务");
-		}else{
+		} else {
 			mAbTitleBar.setTitleText("执法管理-下派执法任务");
 		}
-		mAbTitleBar.setLogo(R.drawable.button_selector_back); 
-//		 设置文字边距，常用来控制高度：
-		 mAbTitleBar.setTitleTextMargin(10, 0, 0, 0);
-//		 设置标题栏背景：
-		 mAbTitleBar.setTitleBarBackground(R.drawable.abg_top); 
-//		 左边图片右边的线：
-		 mAbTitleBar.setLogoLine(R.drawable.aline);
-//		  左边图片的点击事件：
-		 mAbTitleBar.getLogoView().setOnClickListener(new View.OnClickListener() {
-		     @Override
-		     public void onClick(View v) {
-		        finish();
-		     }
+		mAbTitleBar.setLogo(R.drawable.button_selector_back);
+		// 设置文字边距，常用来控制高度：
+		mAbTitleBar.setTitleTextMargin(10, 0, 0, 0);
+		// 设置标题栏背景：
+		mAbTitleBar.setTitleBarBackground(R.drawable.abg_top);
+		// 左边图片右边的线：
+		mAbTitleBar.setLogoLine(R.drawable.aline);
+		// 左边图片的点击事件：
+		mAbTitleBar.getLogoView().setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						finish();
+					}
 
-		 }); 
-		 
+				});
+
 		setAbContentView(R.layout.layout_addzhiliangsend);
 		context = AddZhiliangSendActivity.this;
 		application = (MyApplication) this.getApplication();
 		loginKey = application.getProperty("loginKey");
-		
+		initData();
 		initUi();
-		//加载联系人下拉框
-		if(null==peoples||peoples.size()<=0){
+		// 加载联系人下拉框
+		if (null == peoples || peoples.size() <= 0) {
 			loadPeoples();
-		}else{
+		} else {
 			people_names = new String[peoples.size()];
 			int i = 0;
 			for (People cn : peoples) {
@@ -103,12 +156,45 @@ public class AddZhiliangSendActivity extends AbActivity {
 			}
 			initSpinner();
 		}
-		
+
 	}
 
+	/** 初始化数据 */
+	private void initData() {
+		mVoicesList = new ArrayList<String>();
+		mVoicesListname = new ArrayList<String>();
+		mPlayer = new MediaPlayer();
+	}
+	private void initGridView(){
+		MyGridAdapter mAdapter = new MyGridAdapter(context);
+		addvoicegridview.setAdapter(mAdapter);
+		LayoutParams params = new LayoutParams(mAdapter.getCount()
+				* (cWidth + hSpacing), LayoutParams.WRAP_CONTENT);
+		addvoicegridview.setLayoutParams(params);
+		addvoicegridview.setColumnWidth(cWidth);
+		addvoicegridview.setHorizontalSpacing(hSpacing);
+		addvoicegridview.setStretchMode(GridView.NO_STRETCH);
+		addvoicegridview.setNumColumns(mAdapter.getCount());
+
+		addvoicegridview.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				try {
+					mPlayer.reset();
+					mPlayer.setDataSource(mVoicesList.get(position));
+					mPlayer.prepare();
+					mPlayer.start();
+				} catch (IOException e) {
+					Log.e(TAG, "播放失败");
+				}
+			}
+		});
+	}
 	private void initUi() {
-		
-		//添加
+		horizontalScrollView_addvoice.setHorizontalScrollBarEnabled(true);
+		initGridView();
+		// 添加
 		add.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -116,19 +202,28 @@ public class AddZhiliangSendActivity extends AbActivity {
 				InputMethodManager imm = (InputMethodManager) AddZhiliangSendActivity.this
 						.getSystemService(Context.INPUT_METHOD_SERVICE);
 				imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-				
+
 				submitXiaoxi();
 			}
 		});
-//		search_close.setOnClickListener(new View.OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//				finish();
-//			}
-//		});
-		
-		
+		addVoice.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					startVoice();
+					break;
+				case MotionEvent.ACTION_UP:
+					stopVoice();
+					break;
+				default:
+					break;
+				}
+				return false;
+			}
+		});
+
 		OnClickListener keyboard_hide = new OnClickListener() {
 
 			@Override
@@ -142,6 +237,7 @@ public class AddZhiliangSendActivity extends AbActivity {
 		addxiapai_full.setClickable(true);
 		addxiapai_full.setOnClickListener(keyboard_hide);
 	}
+
 	private void initSpinner() {
 		// 将可选内容与ArrayAdapter连接起来
 		adapter = new ArrayAdapter<String>(AddZhiliangSendActivity.this,
@@ -151,9 +247,9 @@ public class AddZhiliangSendActivity extends AbActivity {
 		// 将adapter 添加到spinner中
 		peopleSpinner.setAdapter(adapter);
 		// 设置默认选中
-//		channelSpinner.setSelection(0);
+		// channelSpinner.setSelection(0);
 		// 设置默认值
-//		channelSpinner.setVisibility(View.VISIBLE);
+		// channelSpinner.setVisibility(View.VISIBLE);
 		peopleSpinner
 				.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
 					@Override
@@ -174,12 +270,14 @@ public class AddZhiliangSendActivity extends AbActivity {
 				});
 
 	}
+
 	/**
 	 * 下发任务
+	 * 
 	 * @author wlj
 	 * @date 2015-6-16下午7:21:20
 	 */
-	private void submitXiaoxi(){
+	private void submitXiaoxi() {
 		// 获取Http工具类
 		final AbHttpUtil mAbHttpUtil = AbHttpUtil.getInstance(this);
 		mAbHttpUtil.setDebug(true);
@@ -187,23 +285,21 @@ public class AddZhiliangSendActivity extends AbActivity {
 			UIHelper.ToastMessage(context, "请检查网络连接");
 			return;
 		}
-		
+
 		AbRequestParams params = new AbRequestParams();
 		params.put("androidNoticeInfo.title", task_tell.getText().toString());
 		params.put("androidNoticeInfo.content", task_info.getText().toString());
 		params.put("androidNoticeInfo.push_user_id", loginKey);
 		params.put("androidNoticeInfo.notice_type", "1");
-		params.put("androidNoticeInfo.receive_user_ids", people+"");
-		Log.d(TAG, String.format("%s?", URLs.ADDMSG,
-				params));
-		mAbHttpUtil.post(URLs.ADDMSG ,params,
+		params.put("androidNoticeInfo.receive_user_ids", people + "");
+		Log.d(TAG, String.format("%s?", URLs.ADDMSG, params));
+		mAbHttpUtil.post(URLs.ADDMSG, params,
 				new AbStringHttpResponseListener() {
 					@Override
 					public void onSuccess(int statusCode, String content) {
 						Log.d(TAG, content);
 						try {
-							QunfaInfo gList = QunfaInfo
-									.parseJson(content);
+							QunfaInfo gList = QunfaInfo.parseJson(content);
 							if (gList.code == 200) {
 								showToast("任务下发成功！");
 								finish();
@@ -234,62 +330,147 @@ public class AddZhiliangSendActivity extends AbActivity {
 					};
 				});
 	}
+
 	@SuppressLint("NewApi")
 	private void loadPeoples() {
-		
+
 		final AbHttpUtil mAbHttpUtil = AbHttpUtil.getInstance(this);
 		if (!application.isNetworkConnected()) {
 			showToast("请检查网络连接");
 			return;
 		}
-		mAbHttpUtil.get(URLs.USERLIST ,
-				new AbStringHttpResponseListener() {
-					// 获取数据成功会调用这里
-					@Override
-					public void onSuccess(int statusCode, String content) {
-						try {
-							PeopleList cList = PeopleList.parseJson(content);
-							if (cList.code == 200) {
-								peoples = cList.getInfo();
-								peoples.remove(Integer.parseInt(loginKey)-1);
-								application.saveObject((Serializable) peoples,"peoples");
-								people_names = new String[peoples.size()];
-								int i = 0;
-								for (People cn : peoples) {
-									people_names[i] = cn.name;
-									i++;
-								}
-								initSpinner();
-							} else {
-								showToast(cList.msg);
-							}
-						} catch (Exception e) {
-							e.printStackTrace();
-							showToast("数据解析失败");
+		mAbHttpUtil.get(URLs.USERLIST, new AbStringHttpResponseListener() {
+			// 获取数据成功会调用这里
+			@Override
+			public void onSuccess(int statusCode, String content) {
+				try {
+					PeopleList cList = PeopleList.parseJson(content);
+					if (cList.code == 200) {
+						peoples = cList.getInfo();
+						peoples.remove(Integer.parseInt(loginKey) - 1);
+						application.saveObject((Serializable) peoples,
+								"peoples");
+						people_names = new String[peoples.size()];
+						int i = 0;
+						for (People cn : peoples) {
+							people_names[i] = cn.name;
+							i++;
 						}
-					};
-
-					// 开始执行前
-					@Override
-					public void onStart() {
-						// 显示进度框
-						showProgressDialog();
+						initSpinner();
+					} else {
+						showToast(cList.msg);
 					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					showToast("数据解析失败");
+				}
+			};
 
-					@Override
-					public void onFailure(int statusCode, String content,
-							Throwable error) {
-						showToast("网络连接失败！");
-					}
+			// 开始执行前
+			@Override
+			public void onStart() {
+				// 显示进度框
+				showProgressDialog();
+			}
 
-					// 完成后调用，失败，成功
-					@Override
-					public void onFinish() {
-						// 移除进度框
-						removeProgressDialog();
-					};
+			@Override
+			public void onFailure(int statusCode, String content,
+					Throwable error) {
+				showToast("网络连接失败！");
+			}
 
-				});
+			// 完成后调用，失败，成功
+			@Override
+			public void onFinish() {
+				// 移除进度框
+				removeProgressDialog();
+			};
+
+		});
+	}
+
+	/** 开始录音 */
+	private void startVoice() {
+		if (mVoicesList.size()>=1) {
+			Toast.makeText(getApplicationContext(), "只能上传一个录音", 0).show();
+			return;
+		}
+		// 设置录音保存路径
+		mFileNameShow=UUID.randomUUID().toString();
+		mFileName = PATH +mFileNameShow + ".amr";
+		String state = android.os.Environment.getExternalStorageState();
+		if (!state.equals(android.os.Environment.MEDIA_MOUNTED)) {
+			Log.i(TAG, "SD Card is not mounted,It is  " + state + ".");
+		}
+		File directory = new File(mFileName).getParentFile();
+		if (!directory.exists() && !directory.mkdirs()) {
+			Log.i(TAG, "Path to file could not be created");
+		}
+		Toast.makeText(getApplicationContext(), "开始录音", 0).show();
+		mRecorder = new MediaRecorder();
+		mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+		mRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+		mRecorder.setOutputFile(mFileName);
+		mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+		try {
+			mRecorder.prepare();
+		} catch (IOException e) {
+			Log.e(TAG, "prepare() failed");
+		}
+		mRecorder.start();
+	}
+
+	/** 停止录音 */
+	@SuppressLint("NewApi")
+	private void stopVoice() {
+		mRecorder.stop();
+		mRecorder.release();
+		mRecorder = null;
+		mVoicesList.add(mFileName);
+		mVoicesListname.add(mFileNameShow);
+//		mVoidListView.deferNotifyDataSetChanged();
+		mAdapter = new MyGridAdapter(AddZhiliangSendActivity.this);
+		addvoicegridview.setAdapter(mAdapter);
+		initGridView();
+		Toast.makeText(getApplicationContext(), "保存录音" + mFileName, 0).show();
+	}
+
+	class MyGridAdapter extends BaseAdapter {
+		Context mContext;
+		LayoutInflater mInflater;
+
+		public MyGridAdapter(Context c) {
+			mContext = c;
+			mInflater = LayoutInflater.from(mContext);
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return mVoicesList.size();
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return arg0;
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			// TODO Auto-generated method stub
+			return arg0;
+		}
+
+		@Override
+		public View getView(int position, View contentView, ViewGroup arg2) {
+			contentView = mInflater.inflate(R.layout.item_voicelist, null);
+			TextView tv = (TextView) contentView.findViewById(R.id.tv_armName);
+			tv.setWidth(400);
+			tv.setText(mVoicesListname.get(position)+ ".amr");
+			return contentView;
+		}
+
 	}
 
 }
