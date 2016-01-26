3b1619ae6168274bcfb2d7f0da09539deaee384d
wanglj7525 <wanglj7525@163.com>
2016-01-26 20:53:33 +0800

    登录页面添加 版本更新提示

--- src/com/ytint/wloaa/activity/LoginActivity.java
+++ src/com/ytint/wloaa/activity/LoginActivity.java
@@ -1,17 +1,25 @@
 package com.ytint.wloaa.activity;
 
+import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.regex.Pattern;
 
 import org.json.JSONObject;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.telephony.TelephonyManager;
+import android.text.Html;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -40,6 +48,9 @@ import com.ytint.wloaa.app.Constants;
 import com.ytint.wloaa.app.MyApplication;
 import com.ytint.wloaa.app.UIHelper;
 import com.ytint.wloaa.bean.URLs;
+import com.ytint.wloaa.bean.Version;
+import com.ytint.wloaa.bean.VersionInfo;
+import com.ytint.wloaa.service.AppUpgradeService;
 import com.ytint.wloaa.utils.Crypto;
 
 /**
@@ -61,6 +72,15 @@ public class LoginActivity extends AbActivity {
 	// 获取Http工具类
 	final AbHttpUtil mAbHttpUtil = AbHttpUtil.getInstance(this);
 	String host;
+	
+	private int mVersionCode;
+	private String mVersionName;
+	private int mLatestVersionCode = 1;
+	private String mLatestVersionUpdate = "mLatestVersionUpdate";
+	private String mLatestVersionDownload = "";
+	private boolean showUpdate = true;
+	private Date lastShowUpdateTime;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -80,7 +100,11 @@ public class LoginActivity extends AbActivity {
 			inputUsername.setText(username);
 		}
 
-		
+		// 检查版本
+		initLocalVersion();
+		if (showUpdate) {
+			getNewVersion();
+		}
 		network.setOnClickListener(new OnClickListener() {
 			
 			@Override
@@ -566,4 +590,131 @@ public class LoginActivity extends AbActivity {
 		JPushInterface.onPause(this);
 	}
 
+	
+	public void initLocalVersion() {
+		PackageInfo pinfo;
+		try {
+			pinfo = this.getPackageManager().getPackageInfo(getPackageName(),
+					PackageManager.GET_CONFIGURATIONS);
+			mVersionCode = pinfo.versionCode;
+			mVersionName = pinfo.versionName;
+	        application.setProperty(Constants.VERSION_NAME, mVersionName);
+	        application.setProperty(Constants.VERSION_CODE, String.valueOf(mVersionCode));
+			String lastDateStr = application.getProperty("showupdatetime");
+			if (null != lastDateStr) {
+				SimpleDateFormat format = new SimpleDateFormat(
+						"yyyy-MM-dd HH:mm:ss");
+				try {
+					lastShowUpdateTime = format.parse(lastDateStr);
+					long diff = new Date().getTime()
+							- lastShowUpdateTime.getTime();
+					if (diff <= Constants.DIFFTIME) {
+						showUpdate = false;
+					}
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+			}
+			Log.e("mVersionCode:", String.valueOf(mVersionCode));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * 获取版本信息
+	 */
+	public void getNewVersion() {
+		if (!application.isNetworkConnected()) {
+			UIHelper.ToastMessage(context, "网络连接失败！");
+			return;
+		} else {
+			mAbHttpUtil.get(host+URLs.GETVERSION + "?id="+mVersionCode, new AbStringHttpResponseListener() {
+				@Override
+				public void onSuccess(int statusCode, String content) {
+					try {
+						VersionInfo vi = VersionInfo.parseJson(content);
+						if (vi.code == 200) {
+							Version v = vi.getInfo();
+							mLatestVersionCode = v.id;
+							mLatestVersionUpdate = "";
+							for (String str : v.introduce) {
+								mLatestVersionUpdate += str + "<br>";
+							}
+							mLatestVersionDownload = v.download_address;
+							checkNewVersion();
+						} else {
+							UIHelper.ToastMessage(context, vi.msg);
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+						UIHelper.ToastMessage(context, "数据解析失败");
+					}
+				}
+
+				@Override
+				public void onFailure(int statusCode, String content,
+						Throwable error) {
+					UIHelper.ToastMessage(context, "网络连接失败！");
+				}
+
+				@Override
+				public void onStart() {
+				}
+
+				// 完成后调用
+				@Override
+				public void onFinish() {
+				};
+			});
+		}
+	}
+
+	/**
+	 * 检查版本
+	 */
+	public void checkNewVersion() {
+		if (mVersionCode < mLatestVersionCode) {
+			SimpleDateFormat format = new SimpleDateFormat(
+					"yyyy-MM-dd HH:mm:ss");
+			String t = format.format(new Date());
+			application.setProperty("showupdatetime", t);
+			new AlertDialog.Builder(this)
+					.setTitle(R.string.check_new_version)
+					.setMessage(Html.fromHtml(mLatestVersionUpdate))
+					.setPositiveButton(R.string.app_upgrade_confirm,
+							new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									Intent intent = new Intent(
+											LoginActivity.this,
+											AppUpgradeService.class);
+									intent.putExtra("downloadUrl",
+											mLatestVersionDownload);
+									startService(intent);
+								}
+							})
+					.setNegativeButton(R.string.app_upgrade_cancel,
+							new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+								}
+							}).create().show();
+		}
+
+		if (mVersionCode >= mLatestVersionCode) {
+			// Toast.makeText(this, R.string.check_new_version_latest,
+			// Toast.LENGTH_SHORT).show();
+			File updateFile = new File(Environment
+					.getExternalStorageDirectory().getPath()
+					+ Constants.DOWNLOADPATH, getResources().getString(
+					R.string.app_name)
+					+ ".apk");
+			if (updateFile.exists()) {
+				// 当不需要的时候，清除之前的下载文件，避免浪费用户空间
+				updateFile.delete();
+			}
+		}
+	}
 }
